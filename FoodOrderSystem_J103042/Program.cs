11887163a5c8fc9527b27c29c91ff3cb628485af
2226179 <2226179@chester.ac.uk>
2025-01-30 22:46:36 +0000

    Updated database connection and EF setup

--- FoodOrderSystem_J103042/Program.cs
+++ FoodOrderSystem_J103042/Program.cs
@@ -1,22 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using FoodOrderSystem_J103042.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<FoodOrderSystem_J103042Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("FoodOrderSystem_J103042Context") ?? throw new InvalidOperationException("Connection string 'FoodOrderSystem_J103042Context' not found.")));
+
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 else
@@ -24,6 +23,8 @@ else
     app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
+
+// Ensure Database Schema is Created
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -33,11 +34,7 @@ using (var scope = app.Services.CreateScope())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
