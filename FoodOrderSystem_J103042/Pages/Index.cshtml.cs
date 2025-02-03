f789f0d641fd77dd7ca1228ae007fed0de28407c
2226179 <2226179@chester.ac.uk>
2025-02-03 19:17:25 +0000

    eod

--- FoodOrderSystem_J103042/Pages/Index.cshtml.cs
+++ FoodOrderSystem_J103042/Pages/Index.cshtml.cs
@@ -1,20 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FoodOrderSystem_J103042.Data;
+using FoodOrderSystem_J103042.Models;
 
 namespace FoodOrderSystem_J103042.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly FoodOrderSystem_J103042Context _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        // Single constructor with both logger and database context
+        public IndexModel(FoodOrderSystem_J103042Context context, ILogger<IndexModel> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        public IList<FoodItem> FoodItem { get; set; } = default!;
 
+        public async Task OnGetAsync()
+        {
+            if (_context.FoodItems != null)
+            {
+                FoodItem = await _context.FoodItems.ToListAsync();
+            }
         }
     }
 }
